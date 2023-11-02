5ecac6160a9613a09d8d9d4ad11b4767902f1c00
Dilshoda <alimovadilshoda47@gmail.com>
2023-11-02 21:29:04 +0500

    Added Container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Button from './components/Button'
 import Showcase from './components/Showcase'
+import Container from './components/Container'
 function App() {
 
   return (
    <div>
-      <Showcase />
+    
    </div>
   )
 }
