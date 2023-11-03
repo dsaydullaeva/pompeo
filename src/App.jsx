67007406a787330b8b4d0ecd89a30eb822ec2a68
Dilshoda <alimovadilshoda47@gmail.com>
2023-11-03 14:55:56 +0500

    added components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import ProductCategories from './components/ProductCategories'
 import OnlineStore from './components/OnlineStore'
 import News from './components/News'
 import Footer from './components/Footer'
+import Shopping from './components/Shopping'
 
 
 function App() {
   return (
    <div>
-
+<Navbar/>
+<Showcase/>
+<ProductCategories/> 
+<OnlineStore/>
+<Shopping/>
+<News/>
+<Footer/> 
    </div>
   )
 }
